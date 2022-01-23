6ca6543d41c60753610bdb904bed24fb957ef753
Rob N ★ <robn@despairlabs.com>
2022-01-23 21:40:24 +1100

    permit v2 files so we can get to work on them!

--- zap.asm
+++ zap.asm
@@ -202,7 +202,7 @@ main:
   lds r16, z_header
   tst r16
   breq PC+3
-  cpi r16, 2 ; no support for v2+
+  cpi r16, 3 ; no support for v3+
   brlo PC+10
 
   ldi ZL, low(text_unsupported_version*2)
